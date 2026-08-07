68d126e63151fa77765d281e91b2cd325e437ea3
JAHNAVI S T <jahnavist5@gmail.com>
2026-08-07 10:03:21 +0530

    Time: 2 ms (47.39%) | Memory: 49.2 MB (5.12%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,43 +1,23 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
-        // Always binary search on the smaller array
-        if (nums1.length > nums2.length)
-            return findMedianSortedArrays(nums2, nums1);
-
-        int m = nums1.length;
-        int n = nums2.length;
-
-        int low = 0;
-        int high = m;
-
-        while (low <= high) {
-
-            int cut1 = (low + high) / 2;
-            int cut2 = (m + n + 1) / 2 - cut1;
-
-            int left1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
-            int right1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
-
-            int left2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
-            int right2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
-
-            if (left1 <= right2 && left2 <= right1) {
-
-                if ((m + n) % 2 == 0) {
-                    return (Math.max(left1, left2) +
-                            Math.min(right1, right2)) / 2.0;
-                } else {
-                    return Math.max(left1, left2);
-                }
-
-            } else if (left1 > right2) {
-                high = cut1 - 1;
-            } else {
-                low = cut1 + 1;
-            }
+        int n1 = nums1.length, n2 = nums2.length;
+        int[] nums3 = new int[n1 + n2];
+        int p1 = 0, p2 = 0, p3 = 0;
+        while (p1 < n1 && p2 < n2) {
+            if (nums1[p1] <= nums2[p2])
+                nums3[p3++] = nums1[p1++];
+            else
+                nums3[p3++] = nums2[p2++];
         }
-
-        return 0.0;
+        while (p1 < n1)
+            nums3[p3++] = nums1[p1++];
+        while (p2 < n2)
+            nums3[p3++] = nums2[p2++];
+        int n = nums3.length;
+        if (n % 2 == 1) {
+            return nums3[n / 2];
+        } else {
+            return (nums3[n / 2 - 1] + nums3[n / 2]) / 2.0;
+        }
     }
 }
